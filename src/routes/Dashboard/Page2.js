112860647ec268c7e526e0d35f6211435e7373a6
Peter Diaz <pjsystem@gmail.com>
2018-05-21 17:56:13 -0500

    learning antd-pro

--- src/routes/Dashboard/Page2.js
+++ src/routes/Dashboard/Page2.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button, notification, Card } from 'antd';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.core.css';
 
-class componentName extends Component {
+export default class NewPage extends React.Component {
+  state = {
+    value: 'test',
+  };
+
+  handleChange = value => {
+    this.setState({
+      value,
+    });
+  };
+
+  prompt = () => {
+    notification.open({
+      message: 'We got value:',
+      description: <span dangerouslySetInnerHTML={{ __html: this.state.value }} />,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>New Layout Page 2</h1>
-      </div>
+      <Card title="Editor de Texto Enriquecido">
+        <ReactQuill value={this.state.value} onChange={this.handleChange} />
+        <Button style={{ marginTop: 16 }} onClick={this.prompt}>
+          Prompt
+        </Button>
+      </Card>
     );
   }
 }
-
-export default componentName;
--- src/utils/request.js
+++ src/utils/request.js
@@ -4,29 +4,30 @@ import { routerRedux } from 'dva/router';
 import store from '../index';
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: 'The server successfully returned the requested data.',
+  201: 'New or modified data is successful.',
+  202: 'A request has entered the background queue (asynchronous task).',
+  204: 'Delete data successfully.',
+  400: 'The request was issued with an error. The server did not create or modify data.',
+  401: 'The user does not have permission (token, username, password error).',
+  403: 'The user is authorized but access is forbidden.',
+  404: 'The request was issued for a non-existent record and the server did not perform the operation.',
+  406: 'The requested format is not available.',
+  410: 'The requested resource is permanently deleted and will no longer be available.',
+  422: 'A validation error occurred when creating an object.',
+  500: 'An error occurred on the server. Please check the server.',
+  502: 'Bad gateway.',
+  503: 'The service is unavailable and the server is temporarily overloaded or maintained.',
+  504: 'Gateway timed out.',
 };
+
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
   const errortext = codeMessage[response.status] || response.statusText;
   notification.error({
-    message: `请求错误 ${response.status}: ${response.url}`,
+    message: `Request Error ${response.status}: ${response.url}`,
     description: errortext,
   });
   const error = new Error(errortext);
