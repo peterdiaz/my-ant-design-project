aa6f373e0317e8e68a2f5d80377ba498721b2803
Peter Diaz <pjsystem@gmail.com>
2018-05-25 14:54:23 -0500

    updating Test

--- src/routes/Dashboard/Test.js
+++ src/routes/Dashboard/Test.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import { Button, notification, Card } from 'antd';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { Card } from 'antd';
 
 export default class NewPage extends React.Component {
-  state = {
-    value: 'test',
-  };
-
-  handleChange = value => {
-    this.setState({
-      value,
-    });
-  };
-
-  prompt = () => {
-    notification.open({
-      message: 'We got value:',
-      description: <span dangerouslySetInnerHTML={{ __html: this.state.value }} />,
-    });
-  };
-
   render() {
     return (
-      <Card title="Editor de Texto Enriquecido">
-        <ReactQuill value={this.state.value} onChange={this.handleChange} />
-        <Button style={{ marginTop: 16 }} onClick={this.prompt}>
-          Prompt
-        </Button>
+      <Card title="dashboard">
+        <div>My Dashboard</div>
       </Card>
     );
   }
